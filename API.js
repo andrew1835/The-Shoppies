5203414683e1d353b5f9190e50b00a5d047ffce5
Andrew Anthony <a.anthony1835@gmail.com>
2021-05-07 14:46:06 -0700

    a remove button now pops up when you add a movie to the nominations list, but the button isn't yet responsive. When you nominate a movie, the nominate button becomes disabled, the opacity goes to 0, and it's replaced by text that says 'Nominated!'

--- API.js
+++ API.js
@@ -21,62 +21,19 @@ searchButton.onclick = function (event) {
         method: "GET"
     }).then(function (response) {
         populateSearchPage(response)
-
-        // for (let i = 0; i < responseData.length; i++) {
-        //     console.log(responseData.Search[i].Title)
-        // }
-        // console.log(responseData.Search[i].Title)
-        // var image = response.Poster
-        // var title = response.Title
-        // var year = response.Year
-        // $(".imageResult").attr("src", image);
-        // $(".titleResult").html('Title: "' + title + '"');
-        // $(".yearResult").html("Year released: " + year);
     });
 }
 
 function populateSearchPage(response) {
+
     responseArray = response
-    // var movieData = document.querySelector("#resultsContainer")
-    // movieData.innerHTML = responseArray.Search.map(function (response) {
-    //     return " <div class='results2'> <img src=" + response.Poster + " alt='No Poster Found'>" + "<h2>" + response.Title + "</h2>" + "<p>" + response.Year + "</p> </div>"
-    // }).join("")
-
-
-
-
-    for (let i = 0; i < 10; i++) {
-        console.log("Title: " + responseArray.Search[i].Title + ". Year: " + responseArray.Search[i].Year)
-        var image = '<img'
-        // console.log(responseArray.Search[i].Type)
-        if (responseArray.Search[i].Type === "movie") {
-            console.log("123MOvie")
-        }
-        else if (responseArray.Search[i].Type === "series") {
-            console.log("123Series")
-        }
-        // Have to create an if statement where if there if poster === N/A, hide the element
-
-
-    }
-
+    // console.log(responseArray.Search)
 
     var movieData = document.querySelector("#resultsContainer")
-    movieData.innerHTML = responseArray.Search.map(function (response) {
-        return " <div class='results'> <img src=" + response.Poster + " class ='moviePoster' alt='No Poster Found'>" + "<h2 class='movieTitle'>" + response.Title + "</h2>" + "<p class='yearReleased'>" + response.Year + "</p> <button class='nominateButton' onclick='nominateMovie(event)'>Nominate</button> </div>"
+    movieData.innerHTML = responseArray.Search.map(function (event) {
+        return " <div class='results'> <img src=" + event.Poster + " class ='moviePoster' alt='No Poster Found'>" + "<h2 class='movieTitle'>" + event.Title + "</h2>" + "<p class='yearReleased'>" + event.Year + "</p> <button class='nominateButton' onclick='nominateMovie(event)'>Nominate</button> <p class='nominated'>Nominated!</p> </div>"
     }).join("")
     addIDs()
-
-
-
-
-
-    // try{throw i}
-    //     catch(ii){
-    //     var movieData = document.querySelector("#resultsContainer")
-    //     movieData.innerHTML = responseArray.Search.map(function (response) {
-    //         return " <div class='results2'> <img src=" + response.Poster + " alt='No Poster Found'>" + "<h2>" + response.Title + "</h2>" + "<p>" + response.Year + "</p> <button class='button' id='button" + ii + "'>Nominate</button> </div>"
-    //     }).join("")}
 
 }
 
@@ -85,34 +42,40 @@ function populateSearchPage(response) {
 
 function addIDs() {
     var yearReleased = document.getElementsByClassName("yearReleased")
+    // console.log(yearReleased)
     var moviePoster = document.getElementsByClassName("moviePoster")
     var nominateButton = document.getElementsByClassName("nominateButton")
     var movieTitle = document.getElementsByClassName("movieTitle")
+    var nominated = document.getElementsByClassName("nominated")
+
     for (let p = 0; p < 10; p++) {
+
         yearReleased[p].setAttribute("id", "p" + p);
         moviePoster[p].setAttribute("id", "img" + p);
         nominateButton[p].setAttribute("id", "btn" + p);
         movieTitle[p].setAttribute("id", "title" + p)
+        nominated[p].setAttribute("id", "nom" + p)
+
     }
 }
 
 
-// z = 0
 
 function nominateMovie(event) {
-    // alert("You nominated me!")
+
     console.log(event.path[0].id)
-    var just = event.path[0].id
-    just = just.substring(3)
-    console.log(just)
+    var buttonID = event.path[0].id
+    var buttonChange = document.getElementById(buttonID)
+    buttonIDNumber = buttonID.substring(3)
+    console.log(buttonIDNumber)
 
-
-    var posterID = document.getElementById("img" + just).src
-    console.log(posterID)
-    var titleID = document.getElementById("title" + just).innerHTML
-    console.log(titleID)
-    var yearID = document.getElementById("p" + just).innerHTML
-    console.log(yearID)
+    var posterSource = document.getElementById("img" + buttonIDNumber).src
+    console.log(posterSource)
+    var titleText = document.getElementById("title" + buttonIDNumber).innerHTML
+    console.log(titleText)
+    var yearText = document.getElementById("p" + buttonIDNumber).innerHTML
+    console.log(yearText)
+    var nominatedID = document.getElementById("nom" + buttonIDNumber)
     var list = document.querySelector("#nominationsList")
     var listLength = document.querySelector("#nominationsList").getElementsByTagName("li").length
 
@@ -121,13 +84,17 @@ function nominateMovie(event) {
     console.log(listArray[0])
 
 
-    if (listLength < 5 && listArray.indexOf(posterID) === -1) {
-        list.innerHTML += "<li>" + titleID + " (" + yearID + ")" + "</li>"
-        listArray.push(posterID)
+    if (listLength < 5 && listArray.indexOf(posterSource) === -1) {
+        list.innerHTML += "<li>" + titleText + " (" + yearText + ")" + "</li> <button class='removeButton' onclick='removeMovie(event)'>Remove</button>"
+        listArray.push(posterSource)
         console.log(listArray)
+        buttonChange.disabled = true
+        buttonChange.style.opacity = '0'
+        buttonChange.style.cursor = 'text'
+        nominatedID.style.display = "block"
     }
-    else if (listArray.indexOf(posterID) !== -1) {
-        alert("already added")
+    else if (listArray.indexOf(posterSource) !== -1) {
+        alert("add a banner when you click the second time")
     }
     else {
         document.getElementById("fiveBanner").style.opacity = "1"
@@ -138,10 +105,12 @@ function nominateMovie(event) {
         }, 2500)
     }
 
-    // At the bottom of this you also want to call another function, which will check to see if the button has been pressed (you can traverse the event to check this, like you did above). Likely the value will be "true". If it's true, then you add styling to the button that makes it unclickable. This is the easy solution, but I don't think it will work on page reload, or if you search something else and then search the original name again. Will think later on if it's possible to make it so that the button is always unclickable, even if you reload the page. You might have to change the code to make the IDs more unique so that instead of an id of "button1" it would be "button" + the URL address. That way, each and every button would have a completely unique ID, and you could make it so that if you click on a button, it adds that unique ID to local storage. A function (which will have to fire after the buttons are created on search) will then check the IDs in local storage, and if there's an ID that matches the IDs of one of the buttons created, that button will not be clickable and will have certain styling applied to it. 
+
 
 
 }
 
+function removeMovie(event) {
 
-// With the above function you've shown that you can create a unique id for each element that you create when you map. You would just have to set the id for each class name of each element. See if you can do this when you click the search button, instead of when you click the nominate button. You would have to call this function after you run the map function though, since if you called it before there wouldn't be any elements to add IDs to
+}
+
